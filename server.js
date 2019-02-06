c1b68cb836a37eeb657a70642d87a448ff662854
JerYoMat <jeremy.emata@gmail.com>
2019-02-06 15:15:29 -0500

    added test for getRates()

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /* Before we start writing our front-end code, we need to implement a server–client base to work from. That means a basic HTML view being served from an Express server. For performance and reliability reasons, we’ll inject front-end dependencies straight from the node_modules folder.*/
 
 require('dotenv').config(); // read .env files
+const { getRates } = require('./lib/fixer-service'); //Added later
 const express = require('express');
 
 const app = express();
@@ -19,3 +20,11 @@ app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 app.listen(port, () => {
   console.log('listening on %d', port);
 });
+
+// Test to verify getRates() is working
+const test = async() => {
+    const data = await getRates();
+    console.log(data);
+  }
+  
+  test();
