2e524a93870ee33b94520c091be3402f5335c75d
JerYoMat <jeremy.emata@gmail.com>
2019-02-06 15:18:28 -0500

    implemented error function, commented out test

--- server.js
+++ server.js
@@ -13,6 +13,32 @@ app.use(express.static('public'));
 // Allow front-end access to node_modules folder
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
+// Express Error handler
+const errorHandler = (err, req, res) => {
+    if (err.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      res.status(403).send({ title: 'Server responded with an error', message: err.message });
+    } else if (err.request) {
+      // The request was made but no response was received
+      res.status(503).send({ title: 'Unable to communicate with server', message: err.message });
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      res.status(500).send({ title: 'An unexpected error occurred', message: err.message });
+    }
+  };
+  
+  // Fetch Latest Currency Rates
+  app.get('/api/rates', async (req, res) => {
+    try {
+      const data = await getRates();
+      res.setHeader('Content-Type', 'application/json');
+      res.send(data);
+    } catch (error) {
+      errorHandler(error, req, res);
+    }
+  });
+
 // Redirect all traffic to index.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
@@ -21,6 +47,8 @@ app.listen(port, () => {
   console.log('listening on %d', port);
 });
 
+
+/*
 // Test to verify getRates() is working
 const test = async() => {
     const data = await getRates();
@@ -28,3 +56,4 @@ const test = async() => {
   }
   
   test();
+  */
